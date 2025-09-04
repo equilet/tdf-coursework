b74bb2c93670ee24f0e68f035631795870fde48d
equilet <jml@dabkitsch.com>
2025-09-04 08:49:32 -0700

    cleanup

--- apds_9960_prox/apds_9960_prox.ino
+++ apds_9960_prox/apds_9960_prox.ino
@@ -1,15 +1,9 @@
 //#include "Particle.h"
 #include "SparkFun_APDS9960.h"
 
-// Define the system setup and log handler
-//SYSTEM_MODE(AUTOMATIC);
-//SYSTEM_THREAD(ENABLED);
-
 SparkFun_APDS9960 apds;
 
 void setup() {
-
-  //interrupts();
 
   Serial.begin(9600);   
 
@@ -19,43 +13,41 @@ void setup() {
   else{
     Serial.println("APDS device initialized!");
   }
-Serial.println("---------------------------");
-    // Start running the APDS-9960 gesture sensor engine
-    if ( apds.enableGestureSensor(true) ) {
+  Serial.println("---------------------------");
+  // Start running the APDS-9960 gesture sensor engine
+  if ( apds.enableGestureSensor(true) ) {
       
-        Serial.println(F("Gesture sensor is now running"));
-    } else {
-        Serial.println(F("Something went wrong during gesture sensor init!"));
-    }
-    if ( apds.enableLightSensor(false) ) {
-        Serial.println(F("Light sensor is now running"));
-    } else {
-        Serial.println(F("Something went wrong during light sensor init!"));
-    }
-    if ( apds.enableProximitySensor(false) ) {
-        Serial.println(F("Proximity sensor is now running"));
-    } else {
-        Serial.println(F("Something went wrong during proximity sensor init!"));
-    }
- // Adjust the Proximity sensor gain
-    if ( !apds.setProximityGain(PGAIN_2X) ) {
-        Serial.println("Something went wrong trying to set PGAIN");
-    }
-    Serial.println("---------------------------");
+      Serial.println(F("Gesture sensor is now running"));
+  } else {
+      Serial.println(F("Something went wrong during gesture sensor init!"));
+  }
+  if ( apds.enableLightSensor(false) ) {
+      Serial.println(F("Light sensor is now running"));
+  } else {
+      Serial.println(F("Something went wrong during light sensor init!"));
+  }
+  if ( apds.enableProximitySensor(false) ) {
+      Serial.println(F("Proximity sensor is now running"));
+  } else {
+      Serial.println(F("Something went wrong during proximity sensor init!"));
+  }
+  // Adjust the Proximity sensor gain
+  if ( !apds.setProximityGain(PGAIN_2X) ) {
+      Serial.println("Something went wrong trying to set PGAIN");
+  }
+  Serial.println("---------------------------");
 
-    delay(700); //  Wait for initialization and calibration to finish
+  delay(700); //  Wait for initialization and calibration to finish
 
 }
 
 void loop() {
-
-  
   // read the proximity value
   uint8_t prox = 0;
   if(!apds.readProximity(prox)){
-   Serial.println("Error reading proximity value");
+    Serial.println("Error reading proximity value");
   }else{
-   Serial.printf("proximity: %d\n", (int)prox);
+     Serial.printf("proximity: %d\n", (int)prox);
   }
 
   delay(100);  //we can read the proximity value faster than the other sensors
